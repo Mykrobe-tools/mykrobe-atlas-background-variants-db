ab1992744fec24f42f401030fbe7d22a43765dbb
Zhicheng-Liu <zl@ebi.ac.uk>
2021-02-05 17:28:22 +0000

    Correct the init db script

--- scripts/init_mongo_db.sh
+++ scripts/init_mongo_db.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-TO_INIT_DB=1
-
-if [ -d "/database/mongo-db" ]
+if [ ! -d "/database/mongo-db" ]
 then
-  TO_INIT_DB=0
-else
-  mkdir -p /database/mongo-db
-fi
-
-mongod --quiet --dbpath /database/mongo-db &
-
-sleep 10
-
-if [ $TO_INIT_DB -eq 1 ]
-then
+  mongod --quiet --dbpath /database/mongo-db &
+  sleep 10
   cp /data/mongodump.tar.gz /database
   cd /database
   tar xzf mongodump.tar.gz
   mongorestore -d mykrobe-mtb mongodump/atlas-mtb
+  sleep 10
+  mongod --shutdown --dbpath /database/mongo-db
+  sleep 10
 fi
+
+mongod --quiet --dbpath /database/mongo-db
